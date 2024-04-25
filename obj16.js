d7e58621810be7e6d0f4116bcb51ee8cae596afb
akfortunate <152958690+akfortunate@users.noreply.github.com>
2024-04-25 18:59:40 +0000

    second commit

--- obj16.js
+++ obj16.js
@@ -3,3 +3,14 @@
 // object.create (this is called constructor method where singleton is created )
 
 // object literals
+
+const jsUser = {
+    name : "Hitesh",
+    age: 18 ,
+    location: "jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLogInDays: ["Monday","Saturday"]
+}
+
+console.log(jsUser.email)
