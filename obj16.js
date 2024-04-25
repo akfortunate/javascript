98fa23024f0a265391b0ec56715feeb579c19996
akfortunate <152958690+akfortunate@users.noreply.github.com>
2024-04-25 18:51:13 +0000

    first commit

---
+++ obj16.js
@@ -0,0 +1,5 @@
+// singleton (if we declare as literals then singleton nahi banta hai, constructors se banega toh singleton hamesha banega singleton)
+
+// object.create (this is called constructor method where singleton is created )
+
+// object literals
