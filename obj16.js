fa98c356090eae631ea48b19ec0d10b6aa1f0e2a
akfortunate <152958690+akfortunate@users.noreply.github.com>
2024-04-27 14:08:34 +0000

    chai aur code

--- obj16.js
+++ obj16.js
@@ -4,8 +4,12 @@
 
 // object literals
 
+const mySym = Symbol("key1")
+
 const jsUser = {
     name : "Hitesh",
+    "full name": "Hitesh choudhary",
+    [mySym] : "mykey1",
     age: 18 ,
     location: "jaipur",
     email: "hitesh@google.com",
@@ -13,4 +17,55 @@ const jsUser = {
     lastLogInDays: ["Monday","Saturday"]
 }
 
-console.log(jsUser.email)
+// console.log(jsUser.full name); syntax error and you cannnot access keys with space 
+// and there is no chance of accessing value full name using dot method
+
+// only method for accessing with spaces is 
+
+//console.log(jsUser["full name"]); for accessing full name this method is used 
+
+// console.log(typeof jsUser.mySym) this is wrong syntax for accessing symbol
+
+//console.log(jsUser[mySym]) this is correct way 
+
+                            // hitesh sir's code 
+
+/*
+ // singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "hitesh@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "hitesh@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo()); */
